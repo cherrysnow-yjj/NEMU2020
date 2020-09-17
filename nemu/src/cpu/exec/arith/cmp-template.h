e5d11bc1992c927682461f15e49b0c97a9419f22
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-17 12:46:32 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:46:32 up 5 days,  5:29,  1 user,  load average: 1.04, 0.45, 0.32 946fe595b2270c38abb0c6619341ab329c7f2661

---
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -0,0 +1,24 @@
+#include "cpu/exec/template-start.h"
+
+#define instr cmp
+
+static void do_execute () {
+	DATA_TYPE result = op_dest->val - op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF = op_dest->val < op_src->val;
+	cpu.SF = result >> len;
+	int s1, s2;
+	s1 = op_dest->val >> len;
+	s2 = op_src->val >> len;
+	cpu.OF = (s1 != s2 && s2 == cpu.SF);
+	cpu.ZF = !result;
+	result ^= result >> 4;
+	result ^= result >> 2;
+	result ^= result >> 1;
+	cpu.PF = !(result & 1);
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+
+#include "cpu/exec/template-end.h"
