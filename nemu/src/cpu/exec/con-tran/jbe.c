e1f856832a41a6d84cbad503bc53937ad35c0e7b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 13:40:42 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:40:42 up 23:36,  1 user,  load average: 0.25, 0.31, 0.28 724d54b66d1a8e5dcb8c2ddf469b9ba065dc63e0

--- nemu/src/cpu/exec/con-tran/jbe.c
+++ nemu/src/cpu/exec/con-tran/jbe.c
@@ -4,4 +4,14 @@
 #include "jbe-template.h"
 #undef DATA_BYTE
 
+#define DATA_BYTE 2
+#include "jbe-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jbe-template.h"
+#undef DATA_BYTE
+
 /* for instruction encoding overloading */
+
+make_helper_v(jbe_i)
