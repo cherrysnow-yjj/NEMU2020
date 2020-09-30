e7b5271030ef512b7a5e0d643b249930a8f97a9b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 07:52:53 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-119-generic #120-Ubuntu SMP Fri Sep 18 11:08:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:52:53 up 1 day, 18:43,  1 user,  load average: 0.42, 0.53, 0.41 d4c2e4344f2663535337e14b3ebf8518f9cea6f3

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -2,15 +2,15 @@
 
 #define instr adc
 
-static void do_execute () {
-	DATA_TYPE result = op_dest->val + op_src->val;
+static void do_execute() {
+	DATA_TYPE result = op_dest->val + op_src->val + cpu.CF;
 	int len = (DATA_BYTE << 3) - 1;
 	int s1, s2;
-	cpu.CF = result < op_dest->val;
+	cpu.CF = (result < op_dest->val);
 	cpu.SF = result >> len;
 	s1 = op_dest->val >> len;
 	s2 = op_src->val >> len;
-	cpu.OF = (s1 == s2 && s1 != cpu.SF);
+    	cpu.OF = (s1 == s2 && s1 != cpu.SF) ;
 	cpu.ZF = !result;
 	OPERAND_W(op_dest, result);
 	result ^= result >> 4;
@@ -19,7 +19,6 @@ static void do_execute () {
 	cpu.PF = !(result & 1);
 	print_asm_template2();
 }
-
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(si2rm)
 #endif
