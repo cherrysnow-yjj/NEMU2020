fc3b79ec8672b1ff5007ebaf223de476ecbc8b6a
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 12:03:11 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-119-generic #120-Ubuntu SMP Fri Sep 18 11:08:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:03:11 up 8 min,  1 user,  load average: 0.51, 0.64, 0.38 55f6657ca2d2089b2653433a96326e9a593134fd

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -2,17 +2,30 @@
 
 #define instr movs
 
-make_helper(concat(movs_, SUFFIX)) {
-	swaddr_write(reg_l(R_EDI), DATA_BYTE, swaddr_read(reg_l(R_ESI), DATA_BYTE));
-	if (cpu.DF == 0) {
-		reg_l(R_EDI) += DATA_BYTE;
-		reg_l(R_ESI) += DATA_BYTE;
+make_helper(concat(movs_l_, SUFFIX)) {
+	if (DATA_BYTE == 2) {
+		swaddr_write(reg_w(R_DI), 2, swaddr_read(reg_l(R_SI), 2));
+		if (cpu.DF == 0) {
+			reg_w(R_DI) += DATA_BYTE;
+			reg_w(R_SI) += DATA_BYTE;
+		}	
+		else {
+			reg_w(R_DI) -= DATA_BYTE;
+			reg_w(R_SI) -= DATA_BYTE;	
+		}
 	}
 	else {
-		reg_l(R_EDI) -= DATA_BYTE;
-		reg_l(R_ESI) -= DATA_BYTE;
+		swaddr_write(reg_l(R_EDI), 4, swaddr_read(reg_l(R_ESI), 4));
+		if (cpu.DF == 0) {
+			reg_l(R_EDI) += DATA_BYTE;
+			reg_l(R_ESI) += DATA_BYTE;
+		}
+		else {
+			reg_l(R_EDI) -= DATA_BYTE;
+			reg_l(R_ESI) -= DATA_BYTE;	
+		}	
 	}
-	print_asm("movs");
+	print_asm_template2();	
 	return 1;
 }
 
--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -48,9 +48,9 @@ make_helper(repnz) {
 	int count = 0;
 	while(cpu.ecx) {
 		exec(eip + 1);
-		count ++;
-		cpu.ecx --;
-		assert(ops_decoded.opcode == 0xa6	// cmpsb
+		count++;
+		cpu.ecx--;
+		assert(ops_decoded.opcode == 0xa6		// cmpsb
 				|| ops_decoded.opcode == 0xa7	// cmpsw
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
