bbf54b9e1344d74c6ff63a82f45f19ced1c7b07f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-05 06:55:48 +0000

    >  run 3018001250 simpleedu Linux simpleedu 4.15.0-119-generic #120-Ubuntu SMP Fri Sep 18 11:08:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:55:48 up 17:02,  1 user,  load average: 0.84, 0.81, 0.49 6c1c1e1affd09fbde7e4f85f78dc968edd88106c

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "FLOAT.h"
-#include <sys/mman.h>
 
 extern char _vfprintf_internal;
 extern char _fpmaxtostr;
-extern char _ppfs_setargs;
 extern int __stdio_fwrite(char *buf, int len, FILE *stream);
 
 __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
@@ -18,47 +16,11 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 	 */
 
 	char buf[80];
-	int sign = f & 0x80000000, len, cd = 0;
-	if (sign) f = (~f) + 1;
-	int hehe = 500000000, i = 15;
-	for (i = 15; i >= 0; i--) {
-		if (f & (1 << i)) cd += hehe;
-		hehe >>= 1;
-	}
-	while (cd > 999999) cd /= 10;
-	if (sign) {
-		len = (sprintf) (buf, "-%d.%06d", ((int)(f) >> 16), cd);
-	} else {
-		len = (sprintf) (buf, "%d.%06d", ((int)(f) >> 16), cd);
-	}
+	int len = sprintf(buf, "0x%08x", f);
 	return __stdio_fwrite(buf, len, stream);
 }
 
 static void modify_vfprintf() {
-	int addr = &_vfprintf_internal;
-	char *sub = (char *)(addr + 0x306 - 0xb);
-	*sub = 0x8;
-	sub = (char *)(addr + 0x306 - 0xa);
-	*sub = 0xff;
-	sub = (char *)(addr + 0x306 - 0x9);
-	*sub = 0x32;
-	sub = (char *)(addr + 0x306 - 0x8);
-	*sub = 0x90;
-
-	sub = (char *)(addr + 0x306 - 30);
-	*sub = 0x90;
-	sub = (char *)(addr + 0x306 - 29);
-	*sub = 0x90;
-	sub = (char *)(addr + 0x306 - 33);
-	*sub = 0x90;
-	sub = (char *)(addr + 0x306 - 34);
-	*sub = 0x90; 
-
-	int *pos = (int *)(addr + 0x307);
-
-	*pos += (int)format_FLOAT - (int)(&_fpmaxtostr);
-	
-	//printf("%x %x %d\n",(int)format_FLOAT,(int)(&_fpmaxtostr),(int)format_FLOAT - (int)(&_fpmaxtostr));
 	/* TODO: Implement this function to hijack the formating of "%f"
 	 * argument during the execution of `_vfprintf_internal'. Below
 	 * is the code section in _vfprintf_internal() relative to the
@@ -105,19 +67,6 @@ static void modify_vfprintf() {
 }
 
 static void modify_ppfs_setargs() {
-	int addr = &_ppfs_setargs;
-
-	//mprotect((void *)((addr + 0x73 - 0x64) & 0xfffff000), 4096 * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
-
-	char *pos = (char *)(addr + 0x71);
-	*pos = 0xeb;
-	pos = (char *)(addr + 0x72);
-	*pos = 0x30;
-	pos = (char *)(addr + 0x73);
-	*pos = 0x90;
-	//guess guess guess what???
-
-	
 	/* TODO: Implement this function to modify the action of preparing
 	 * "%f" arguments for _vfprintf_internal() in _ppfs_setargs().
 	 * Below is the code section in _vfprintf_internal() relative to
@@ -126,14 +75,14 @@ static void modify_ppfs_setargs() {
 
 #if 0
 	enum {                          /* C type: */
-		PA_INT,  0                     /* int */
+		PA_INT,                       /* int */
 		PA_CHAR,                      /* int, cast to char */
 		PA_WCHAR,                     /* wide char */
 		PA_STRING,                    /* const char *, a '\0'-terminated string */
 		PA_WSTRING,                   /* const wchar_t *, wide character string */
 		PA_POINTER,                   /* void * */
 		PA_FLOAT,                     /* float */
-		PA_DOUBLE, 7                   /* double */
+		PA_DOUBLE,                    /* double */
 		__PA_NOARG,                   /* non-glibc -- signals non-arg width or prec */
 		PA_LAST
 	};
