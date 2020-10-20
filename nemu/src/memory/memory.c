d6a927790bbb9fc5a713f8fb7e0dd3142a1afcb7
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 12:23:35 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:23:35 up  2:59,  1 user,  load average: 1.07, 1.09, 0.92 8789b5a4579b874ab6ee2abe81904dff4b76f99f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,37 +1,60 @@
 #include "common.h"
+#include "burst.h"
+#include "nemu.h"
+#include "memory/cache.h"
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 
-uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+uint32_t hwaddr_read(hwaddr_t addr, size_t len)
+{
+	int id = cache_read(addr);
+	uint32_t offset = addr & (CACHE_BLOCK_SIZE - 1);
+	uint8_t tmp[2 * BURST_LEN];
+	if (offset + len > CACHE_BLOCK_SIZE)
+	{
+		int id2 = cache_read(addr + CACHE_BLOCK_SIZE - offset);
+		memcpy(tmp, cache[id].data + offset, CACHE_BLOCK_SIZE - offset);
+		memcpy(tmp + CACHE_BLOCK_SIZE - offset, cache[id2].data, len - (CACHE_BLOCK_SIZE - offset));
+	}
+	else
+	{
+		memcpy(tmp, cache[id].data + offset, len);
+	}
+	int zero = 0;
+	uint32_t ans = unalign_rw(tmp + zero, 4) & (~0u >> ((4 - len) << 3));
+	return ans;
 }
 
-void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
+void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data)
+{
 	dram_write(addr, len, data);
 }
 
-uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
+uint32_t lnaddr_read(lnaddr_t addr, size_t len)
+{
 	return hwaddr_read(addr, len);
 }
 
-void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
+void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data)
+{
 	hwaddr_write(addr, len, data);
 }
 
-uint32_t swaddr_read(swaddr_t addr, size_t len) {
+uint32_t swaddr_read(swaddr_t addr, size_t len)
+{
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
 	return lnaddr_read(addr, len);
 }
 
-void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
+void swaddr_write(swaddr_t addr, size_t len, uint32_t data)
+{
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
 	lnaddr_write(addr, len, data);
 }
-
