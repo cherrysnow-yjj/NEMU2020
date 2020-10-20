3e4208d5fb7d55957a520fcb35da7bf9abe1fa75
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 13:52:06 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:52:06 up 15 min,  1 user,  load average: 1.33, 0.98, 0.59 7a1aaa9eb84f6b749e61862dc2317f02835fa9e

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -15,6 +15,10 @@ void init_cache()
         cache[i].valid = false;
     }
     // printf("check\n");
+    for (i = 0; i < SECONDCACHE_WAY_SIZE * SECONDCACHE_SET_SIZE; i++)
+    {
+        secondcache[i].valid = false;
+    }
 }
 
 uint32_t cache_read(hwaddr_t addr)
@@ -22,7 +26,7 @@ uint32_t cache_read(hwaddr_t addr)
     uint32_t tag = addr >> (CACHE_SET_SIZE_B + CACHE_BLOCK_SIZE_B);
     uint32_t set = addr >> (CACHE_BLOCK_SIZE_B);
     set &= (CACHE_SET_SIZE - 1);
-    uint32_t block = (addr >> CACHE_BLOCK_SIZE_B) << CACHE_BLOCK_SIZE_B;
+    // uint32_t block = (addr >> CACHE_BLOCK_SIZE_B) << CACHE_BLOCK_SIZE_B;
 
     int i;
     for (i = set * CACHE_WAY_SIZE; i < (set + 1) * CACHE_WAY_SIZE; i++)
@@ -33,13 +37,18 @@ uint32_t cache_read(hwaddr_t addr)
         }
     }
 
+    int j = secondcache_read(addr);
     srand(i);
     i = CACHE_WAY_SIZE * set + rand() % CACHE_WAY_SIZE;
-    int j;
-    for (j = 0; j < CACHE_BLOCK_SIZE / BURST_LEN; j++)
-    {
-        ddr3read(block + j * BURST_LEN, cache[i].data + j * BURST_LEN);
-    }
+    memcpy(cache[i].data, secondcache[j].data, CACHE_BLOCK_SIZE);
+
+    // srand(i);
+    // i = CACHE_WAY_SIZE * set + rand() % CACHE_WAY_SIZE;
+    // int j;
+    // for (j = 0; j < CACHE_BLOCK_SIZE / BURST_LEN; j++)
+    // {
+    //     ddr3read(block + j * BURST_LEN, cache[i].data + j * BURST_LEN);
+    // }
 
     cache[i].valid = true;
     cache[i].tag = tag;
@@ -63,20 +72,95 @@ void cache_write(hwaddr_t addr, size_t len, uint32_t data)
         {
             if (offset + len > CACHE_BLOCK_SIZE)
             {
-                dram_write(addr, CACHE_BLOCK_SIZE - offset, data); // write through
+                // dram_write(addr, CACHE_BLOCK_SIZE - offset, data); // write through
                 memcpy(cache[i].data + offset, &data, CACHE_BLOCK_SIZE - offset);
+                secondcache_write(addr, CACHE_BLOCK_SIZE - offset, data);
                 cache_write(addr + CACHE_BLOCK_SIZE - offset, len - (CACHE_BLOCK_SIZE - offset), data >> (CACHE_BLOCK_SIZE - offset));
             }
             else
             {
-                dram_write(addr, len, data); // write through
+                // dram_write(addr, len, data); // write through
                 memcpy(cache[i].data + offset, &data, len);
+                secondcache_write(addr, len, data);
             }
             return;
         }
     }
 
-    dram_write(addr, len, data);
+    secondcache_write(addr, len, data);
 
     // printf("check\n");
+}
+
+uint32_t secondcache_read(hwaddr_t addr)
+{
+    uint32_t tag = addr >> (SECONDCACHE_SET_SIZE_B + SECONDCACHE_BLOCK_SIZE_B);
+    uint32_t set = addr >> (SECONDCACHE_BLOCK_SIZE_B);
+    set &= (SECONDCACHE_SET_SIZE - 1);
+    uint32_t block = (addr >> SECONDCACHE_BLOCK_SIZE_B) << SECONDCACHE_BLOCK_SIZE_B;
+
+    int i;
+    for (i = set * SECONDCACHE_WAY_SIZE; i < (set + 1) * SECONDCACHE_WAY_SIZE; i++)
+    {
+        if (secondcache[i].valid && secondcache[i].tag == tag)
+        {
+            return i;
+        }
+    }
+    // hit miss
+    srand(i);
+    i = SECONDCACHE_WAY_SIZE * set + rand() % SECONDCACHE_WAY_SIZE;
+    if (secondcache[i].dirty && secondcache[i].valid)
+    {
+        uint32_t addr2 = (secondcache[i].tag << (SECONDCACHE_SET_SIZE_B + SECONDCACHE_BLOCK_SIZE_B)) | (set << (SECONDCACHE_BLOCK_SIZE_B));
+        uint8_t mask[2 * BURST_LEN];
+        memset(mask, 1, 2 * BURST_LEN);
+        int j;
+        for (j = 0; j < SECONDCACHE_BLOCK_SIZE / BURST_LEN; j++)
+        {
+            ddr3write(addr2 + j * BURST_LEN, secondcache[i].data + j * BURST_LEN, mask);
+        }
+    }
+    int j;
+    for (j = 0; j < SECONDCACHE_BLOCK_SIZE / BURST_LEN; j++)
+    {
+        ddr3read(block + j * BURST_LEN, secondcache[i].data + j * BURST_LEN);
+    }
+
+    secondcache[i].valid = true;
+    secondcache[i].tag = tag;
+    secondcache[i].dirty = false;
+
+    return i;
+}
+
+void secondcache_write(hwaddr_t addr, size_t len, uint32_t data)
+{
+    uint32_t tag = addr >> (SECONDCACHE_SET_SIZE_B + SECONDCACHE_BLOCK_SIZE_B);
+    uint32_t set = addr >> (SECONDCACHE_BLOCK_SIZE_B);
+    set &= (SECONDCACHE_SET_SIZE - 1);
+    uint32_t offset = addr & (SECONDCACHE_BLOCK_SIZE - 1);
+
+    int i;
+    for (i = set * SECONDCACHE_WAY_SIZE; i < (set + 1) * SECONDCACHE_WAY_SIZE; i++)
+    {
+        if (secondcache[i].valid && secondcache[i].tag == tag)
+        {
+            secondcache[i].dirty = true;
+            if (offset + len > SECONDCACHE_BLOCK_SIZE)
+            {
+                memcpy(secondcache[i].data + offset, &data, SECONDCACHE_BLOCK_SIZE - offset);
+                secondcache_write(addr + SECONDCACHE_BLOCK_SIZE - offset, len - (SECONDCACHE_BLOCK_SIZE - offset), data >> (SECONDCACHE_BLOCK_SIZE - offset));
+            }
+            else
+            {
+                memcpy(secondcache[i].data + offset, &data, len);
+            }
+            return;
+        }
+    }
+
+    int j = secondcache_read(addr);
+    secondcache[j].dirty = true;
+    memcpy(secondcache[j].data + offset, &data, len);
 }
