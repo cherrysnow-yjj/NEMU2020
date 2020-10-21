cfbe3873faf78b4f3adb60010c52596936939fda
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 15:56:33 +0000

    >  compile kernel 3018001250 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:56:33 up 1 day,  2:19,  1 user,  load average: 1.27, 0.94, 0.85 7645d9083028c6a990409bca17a611bf45022092

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -16,7 +16,8 @@ void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 void create_video_mapping();
 uint32_t get_ucr3();
 
-uint32_t loader() {
+uint32_t loader()
+{
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
@@ -28,7 +29,7 @@ uint32_t loader() {
 	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #endif
 
-	elf = (void*)buf;
+	elf = (void *)buf;
 
 	/* TODO: fix the magic number with the correct one */
 	const uint32_t elf_magic = 0x464c457f;
@@ -37,25 +38,31 @@ uint32_t loader() {
 
 	/* Load each program segment */
 	int i;
-	ph = (void*)(buf+elf->e_phoff);
-	for (i = 0; i < elf->e_phnum; i++, ph++) {
+	ph = (void *)(buf + elf->e_phoff);
+	for (i = 0; i < elf->e_phnum; i++, ph++)
+	{
 		/* Scan the program header table, load each segment into memory */
-		if (ph->p_type == PT_LOAD) {
+		if (ph->p_type == PT_LOAD)
+		{
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			ramdisk_read((void*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
-			 
+			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
+
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void*)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz-ph->p_filesz);
+			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
+			if (cur_brk < new_brk)
+			{
+				max_brk = cur_brk = new_brk;
+			}
 #endif
 		}
 	}
