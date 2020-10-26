c751889e42cc24f5653a2a6ce3e453699d762878
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-26 13:14:58 +0000

    >  compile kernel 3018001250 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:14:58 up 31 min,  1 user,  load average: 0.33, 0.97, 0.79 3435fccd7c9249914ac03440c541616ef5755b23

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -6,34 +6,48 @@
 #define SCR_SIZE (320 * 200)
 
 /* Use the function to get the start address of user page directory. */
-PDE* get_updir();
+PDE *get_updir();
+static PTE vtable[1024] align_to_page;
 
-void create_video_mapping() {
+void create_video_mapping()
+{
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	PDE *tmp = (PDE *)va_to_pa(get_updir());
+	tmp[0].page_frame = (uint32_t)va_to_pa(vtable) >> 12;
+	tmp[0].present = 1;
+	int i;
+	for (i = VMEM_ADDR / PAGE_SIZE; i < (VMEM_ADDR + SCR_SIZE) / PAGE_SIZE; i++)
+	{
+		vtable[i].page_frame = i;
+		vtable[i].present = 1;
+	}
 }
 
-void video_mapping_write_test() {
+void video_mapping_write_test()
+{
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
-	for(i = 0; i < SCR_SIZE / 4; i ++) {
+	for (i = 0; i < SCR_SIZE / 4; i++)
+	{
 		buf[i] = i;
 	}
 }
 
-void video_mapping_read_test() {
+void video_mapping_read_test()
+{
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
-	for(i = 0; i < SCR_SIZE / 4; i ++) {
+	for (i = 0; i < SCR_SIZE / 4; i++)
+	{
 		assert(buf[i] == i);
 	}
 }
 
-void video_mapping_clear() {
+void video_mapping_clear()
+{
 	memset((void *)VMEM_ADDR, 0, SCR_SIZE);
 }
-
