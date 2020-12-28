c373f6b01ed4f7e0cee8bfc21e717254f9fc656e
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 11:54:39 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:54:39 up  1:31,  1 user,  load average: 1.59, 1.48, 1.02 cc44a6f93e8b2c472d2fa167bbcd57a1ff5d9c87

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -2,22 +2,37 @@
 
 #define instr test
 
-static void do_execute() {
-	DATA_TYPE result = op_dest->val & op_src->val;
-	int len = (DATA_BYTE << 3) - 1; // 31
+static void do_execute()
+{
 	cpu.CF = 0;
 	cpu.OF = 0;
-	cpu.SF = result >> len;
-	cpu.ZF = !result;
-	result ^= result >> 4;
-	result ^= result >> 2;
-	result ^= result >> 1;
-	cpu.PF = !(result & 1);
+	int ans = op_dest->val & op_src->val;
+	if (ans < 0)
+		cpu.SF = 1;
+	else
+		cpu.SF = 0;
+	if (ans == 0)
+		cpu.ZF = 1;
+	else
+		cpu.ZF = 0;
+	//printf("%d\n",cpu.ZF);
+	int cnt = 0;
+	ans &= 0xff;
+	while (ans)
+	{
+		ans = ans & (ans - 1);
+		cnt++;
+	}
+	if (cnt % 2)
+	{
+		cpu.PF = 0;
+	}
+	else
+		cpu.PF = 1;
 	print_asm_template2();
 }
 
-make_instr_helper(i2a)
-make_instr_helper(i2rm)
-make_instr_helper(r2rm)
-
+make_instr_helper(r2rm);
+make_instr_helper(i2rm);
+make_instr_helper(i2a);
 #include "cpu/exec/template-end.h"
