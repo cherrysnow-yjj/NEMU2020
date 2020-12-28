3aba419b0643d52f71b8108cbf33c0fd9659692d
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 07:09:49 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-126-generic #129-Ubuntu SMP Mon Nov 23 18:53:38 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:09:49 up  1:25,  1 user,  load average: 1.71, 1.19, 0.72 7d185ac661aecd22cd2a591d88ab80fb99f7e706

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -5,7 +5,8 @@
 /* There are no more than 16(actually, 3) kinds of hardward interrupts. */
 #define NR_HARD_INTR 16
 
-struct IRQ_t {
+struct IRQ_t
+{
 	void (*routine)(void);
 	struct IRQ_t *next;
 };
@@ -16,42 +17,50 @@ static int handle_count = 0;
 
 void do_syscall(TrapFrame *);
 
-void
-add_irq_handle(int irq, void (*func)(void) ) {
+void add_irq_handle(int irq, void (*func)(void))
+{
 	assert(irq < NR_HARD_INTR);
 	assert(handle_count <= NR_IRQ_HANDLE);
 
 	struct IRQ_t *ptr;
-	ptr = &handle_pool[handle_count ++]; /* get a free handler */
+	ptr = &handle_pool[handle_count++]; /* get a free handler */
 	ptr->routine = func;
 	ptr->next = handles[irq]; /* insert into the linked list */
 	handles[irq] = ptr;
 }
 
-void irq_handle(TrapFrame *tf) {
+void irq_handle(TrapFrame *tf)
+{
 	/* TODO: Re-organize the `TrapFrame' structure in `include/irq.h'
 	 * to match the trap frame built during `do_irq.S'. Remove the
 	 * following line after you are done.
 	 */
-	panic("Have you re-organized the `TrapFrame' structure?");
+	// panic("Have you re-organized the `TrapFrame' structure?");
 
 	int irq = tf->irq;
 
-	if (irq < 0) {
+	if (irq < 0)
+	{
 		panic("Unhandled exception!");
-	} else if (irq == 0x80) {
+	}
+	else if (irq == 0x80)
+	{
 		do_syscall(tf);
-	} else if (irq < 1000) {
+	}
+	else if (irq < 1000)
+	{
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
-	} else if (irq >= 1000) {
+	}
+	else if (irq >= 1000)
+	{
 		int irq_id = irq - 1000;
 		assert(irq_id < NR_HARD_INTR);
 		struct IRQ_t *f = handles[irq_id];
 
-		while (f != NULL) { /* call handlers one by one */
-			f->routine(); 
+		while (f != NULL)
+		{ /* call handlers one by one */
+			f->routine();
 			f = f->next;
 		}
 	}
 }
-
