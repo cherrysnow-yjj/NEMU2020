4c0480108136486dbb243bcdf59a5d6123bda9e1
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 07:20:45 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-126-generic #129-Ubuntu SMP Mon Nov 23 18:53:38 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:20:45 up  1:36,  1 user,  load average: 1.98, 1.41, 1.02 e97578897d18630245d04313ad24c785bd248f1d

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -5,34 +5,69 @@
 void add_irq_handle(int, void (*)(void));
 uint32_t mm_brk(uint32_t);
 int fs_ioctl(int, uint32_t, void *);
+void serial_printc(char);
 
-static void sys_brk(TrapFrame *tf) {
+static void sys_brk(TrapFrame *tf)
+{
 	tf->eax = mm_brk(tf->ebx);
 }
 
-static void sys_ioctl(TrapFrame *tf) {
+static void sys_write(TrapFrame *tf)
+{
+	uint32_t fd = tf->ebx;
+	char *buf = (char *)tf->ecx;
+	uint32_t len = tf->edx;
+	uint32_t ans = 0;
+	if (fd == 1 || fd == 2)
+	{
+		//set_bp();
+		//asm volatile (".byte 0xd6" :: "a"(2), "c"(buf), "d"(len));
+		while (len--)
+			serial_printc(*(buf++));
+		ans = tf->edx;
+		//set_bp();
+	}
+	else
+	{
+		//ans = fs_write(fd, buf, len);
+		ans = 210;
+	}
+	tf->eax = ans;
+}
+
+static void sys_ioctl(TrapFrame *tf)
+{
 	tf->eax = fs_ioctl(tf->ebx, tf->ecx, (void *)tf->edx);
 }
 
-void do_syscall(TrapFrame *tf) {
-	switch(tf->eax) {
-		/* The `add_irq_handle' system call is artificial. We use it to
+void do_syscall(TrapFrame *tf)
+{
+	switch (tf->eax)
+	{
+	/* The `add_irq_handle' system call is artificial. We use it to
 		 * let user program register its interrupt handlers. But this is
 		 * very dangerous in a real operating system. Therefore such a
 		 * system call never exists in GNU/Linux.
 		 */
-		case 0: 
-			cli();
-			add_irq_handle(tf->ebx, (void*)tf->ecx);
-			sti();
-			break;
+	case 0:
+		cli();
+		add_irq_handle(tf->ebx, (void *)tf->ecx);
+		sti();
+		break;
 
-		case SYS_brk: sys_brk(tf); break;
-		case SYS_ioctl: sys_ioctl(tf); break;
+	case SYS_brk:
+		sys_brk(tf);
+		break;
+	case SYS_ioctl:
+		sys_ioctl(tf);
+		break;
+	case SYS_write:
+		sys_write(tf);
+		break;
 
 		/* TODO: Add more system calls. */
 
-		default: panic("Unhandled system call: id = %d, eip = 0x%08x", tf->eax, tf->eip);
+	default:
+		panic("Unhandled system call: id = %d, eip = 0x%08x", tf->eax, tf->eip);
 	}
 }
-
