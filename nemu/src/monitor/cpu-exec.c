31a69fac2d731eb424a3f0c237421c3f112128dd
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-29 10:31:13 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:31:13 up 45 min,  1 user,  load average: 1.17, 1.12, 0.99 933861c56dbff0e8498b276580e130d9129d09f9

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -90,8 +90,8 @@ void cpu_exec(volatile uint32_t n)
 
 		/* TODO: check watchpoints here. */
 		printf("c1%x\n",nemu_state);
-		if (check_wp())
-			nemu_state = STOP;
+		/*if (check_wp())
+			nemu_state = STOP;*/
 		printf("c2%x\n",nemu_state);
 #ifdef HAS_DEVICE
 		extern void device_update();
