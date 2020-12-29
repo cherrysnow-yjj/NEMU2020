e981b2d1e9e987fa9ea8d2a0b68d8ec22b542cb3
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-29 10:27:16 +0000

    >  compile NEMU 3018001250 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:27:16 up 41 min,  1 user,  load average: 2.24, 1.40, 1.04 3b0be769245ccf0d1f9aba22b33f23cba22e1679

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -68,16 +68,16 @@ void cpu_exec(volatile uint32_t n)
 			fputc('.', stderr);
 		}
 #endif
-
+		printf("cc%x\n",n);
 		/* Execute one instruction, including instruction fetch,
 		 * instruction decode, and the actual execution. */
-		//printf("%x\n",cpu.eip);
+		printf("%x\n",cpu.eip);
 		int instr_len = exec(cpu.eip);
-		//printf("%x\n",instr_len);
-		//printf("3\n");
+		printf("%x\n",instr_len);
+		printf("3\n");
 		cpu.eip += instr_len;
 		//printf("4\n");
-		//printf("%x\n",cpu.eip);
+		printf("%x\n",cpu.eip);
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
